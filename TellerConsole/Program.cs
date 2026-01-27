53e067c65c3178b8f268879d503d40c3135ed832
George Branch <gebranch@att.net>
2026-01-27 16:47:37 -0500

    Changed inline Program calls to function calls.

--- TellerConsole/Program.cs
+++ TellerConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Autofac;
 using Autofac.Core;
 using AutoMapper;
+using Microsoft.Identity.Client;
 using Serilog;
 using Serilog.Extensions.Logging;
 using TellerDomain;
@@ -17,18 +18,10 @@ Log.Logger = new LoggerConfiguration()
     .CreateLogger();
 
 var builder = new ContainerBuilder();
-builder.RegisterInstance(_functions.GetMapper()).As<IMapper>().SingleInstance();
-builder.RegisterType<Deposit>();
-builder.RegisterType<Withdraw>();
+var container = _functions.RegisterDependencies(builder);
 
-builder.RegisterAssemblyTypes(typeof(Program).Assembly)
-       .Where(t => t.IsSubclassOf(typeof(Transaction)))
-       .AsSelf();
-
-var container = builder.Build();
-
-Deposit deposit;
-Withdraw withdraw;
+Deposit? deposit = null;
+Withdraw? withdraw = null;
 IMapper mapper;
 using var scope = container.BeginLifetimeScope();
 try
@@ -50,42 +43,42 @@ while (runProcess == true)
     {
         var transaction = new Transaction();
 
-        runProcess = true;
-
         Console.WriteLine("_____________________________");
         Console.WriteLine("Processing new transaction...");
         Console.WriteLine("_____________________________");
 
         do
         {
-            Console.Write("Enter the Account Number or Q for quit: ");
-            var acctNumber = Console.ReadLine();
-            if (string.IsNullOrWhiteSpace(acctNumber))
+            var success = _functions.GetAccountNumber(transaction);
+            if (transaction.QuitProgram)
             {
+                runProcess = false;
+                break;
+            }
+            if (!success)
+            {
+                Log.Error($"Unable to get account number {transaction.AccountNumber}");
+                Console.WriteLine($"Unable to get account number {transaction.AccountNumber}");
                 runProcess = false;
                 continue;
             }
-            if (acctNumber.ToUpper() == "Q")
+
+            success = _functions.GetAccountType(transaction);
+            if (transaction.QuitProgram)
             {
                 runProcess = false;
-                continue;
+                break;
             }
-            transaction.AccountNumber = Convert.ToInt32(acctNumber);
-
-            Console.Write("Enter the Account Type: (Checking = 1, Savings = 2): ");
-            transaction.AccountType = (AccountType)Int32.Parse(Console.ReadLine());
-
-            if (!transaction.AccountType.IsValidAccountType())
-            {
-                Log.Error($"Invalid account type {transaction.AccountType}. Please enter 1 for Checking or 2 for Savings.");
-                Console.WriteLine($"Invalid account type {transaction.AccountType}. Please enter 1 for Checking or 2 for Savings.");
+            if (!success) {
+                Log.Error($"Unable to get account type for account Type {transaction.AccountType}");
+                Console.WriteLine($"Unable to get account type for account Type {transaction.AccountType}");
                 runProcess = false;
                 continue;
             }
 
             try
             {
-                transaction = _functions.GetMemberByAccountNumberAndType(transaction);
+                success = _functions.GetMemberByAccountNumberAndType(transaction);
             }
             catch (InvalidDataException ex)
             {
@@ -94,32 +87,31 @@ while (runProcess == true)
                 continue;
             }
 
-            transaction.OriginalAccountBalance = transaction.AccountDTO.Balance;
-
-
-            Console.Write("Enter the Transaction Type (Deposit = 1, Withdrawal = 2): ");
-            transaction.TransactionType = (TransactionType)Int32.Parse(Console.ReadLine());
-
-            if (!transaction.TransactionType.IsValidTransactionType())
+            success = _functions.GetTransactionType(transaction);
+            if (transaction.QuitProgram)
             {
-                Console.WriteLine($"{transaction.TransactionType} is not a valid transaction type");
-                Log.Error($"{transaction.TransactionType} is not a valid transaction type");
-                continue;
+                runProcess = false;
+                break;
             }
-
-            Console.Write("Enter the Amount: ");
-            var tranAmount = Console.ReadLine();
-            if (string.IsNullOrWhiteSpace(tranAmount))
+            if (!success)
             {
-                Log.Error($"Account: {transaction.MemberDTO?.AccountNumber} - Invalid deposit amount: <= zero: {tranAmount}");
+                Log.Error($"Unable to get transaction type for transaction Type {transaction.TransactionType}");
+                Console.WriteLine($"Unable to get transaction type for transaction Type {transaction.TransactionType}");
                 runProcess = false;
                 continue;
             }
-            transaction.AmountToProcess = Convert.ToDecimal(tranAmount);
-            if (transaction.AmountToProcess <= 0)
+
+            success = _functions.GetTransactionAmount(transaction);
+            if (transaction.QuitProgram)
             {
-                Log.Error($"Account: {transaction.MemberDTO?.AccountNumber} - Invalid deposit amount: <= zero: {transaction.AmountToProcess}");
-                Console.WriteLine("Deposit amount must be greater than zero.");
+                runProcess = false;
+                break;
+            }
+            if (!success)
+            {
+                Log.Error($"Unable to get transaction amount for amount {transaction.AmountToProcess}");
+                Console.WriteLine($"Unable to get transaction amount for amount {transaction.AmountToProcess}");
+                runProcess = false;
                 continue;
             }
 
--- TellerDomain/Transactions/Transaction.cs
+++ TellerDomain/Transactions/Transaction.cs
@@ -3,14 +3,15 @@ namespace TellerDomain
 {
     public class Transaction
     {
-        public AccountType AccountType { get; set; }
-        public TransactionType TransactionType { get; set; }
+        public AccountType AccountType { get; set; } = AccountType.Undefined;
+        public TransactionType TransactionType { get; set; } = TransactionType.Undefined;
         public MemberDTO? MemberDTO { get; set; } = new MemberDTO();
         public AccountDTO? AccountDTO { get; set; } = new AccountDTO();
-        public decimal AmountToProcess { get; set; }
-        public decimal OriginalAccountBalance { get; set; }
-        public int AccountNumber { get; set; }
-        public int AccountId { get; set; }
+        public decimal AmountToProcess { get; set; } = 0;
+        public decimal OriginalAccountBalance { get; set; } = 0;
+        public int AccountNumber { get; set; } = 0;
+        public int AccountId { get; set; } = 0;
+        public bool QuitProgram { get; set; } = false;
         public virtual Transaction ProcessTransaction(Transaction transaction) 
         {
             throw new NotImplementedException();
